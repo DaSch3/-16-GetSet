00359b40548bf6f398b571e2028a400d070725dc
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:36:15 +0200

    Projektdateien hinzufügen.

---
+++ GetSet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person();
+
+            peter.Alter = 10;
+            Console.WriteLine(peter.Alter);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        //Private Variablen
+        private int alter;
+
+        //Eigenschaften
+        public int Alter
+        {
+            get
+            {
+                Console.WriteLine("Alter wurde gelesen");
+                return alter;
+            }
+            set
+            {
+                Console.WriteLine("Alter wurde überschrieben");
+                alter = value;
+            }
+        }
+    }
+}
